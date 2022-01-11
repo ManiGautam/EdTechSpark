ff5728c1a7b4fb14f74c2170fadbe4a4bcebae96
manijee <inboxtomanijee@gmail.com>
2022-01-11 16:16:36 +0530

    Initial commit with splash

---
+++ app/src/main/java/com/manijee/edtechspark/views/activities/SplashScreen.java
@@ -0,0 +1,16 @@
+package com.manijee.edtechspark.views.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.manijee.edtechspark.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
